bcc90acb62c33e79ca618fe8d8939beac1a3c071
Makday <58140558+Makday@users.noreply.github.com>
2025-09-08 03:42:53 +0300

    add project template

---
+++ TodoApp/Models/TodoTask.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class TodoTask
+    {
+        public Guid Id { get; set; }
+        public bool IsCompleted { get; set; }
+        [Required] public string TaskName { get; set; }
+        public DateTimeOffset? DueDate { get; set; }
+    }
+}
---
+++ TodoApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using TodoApp.Client.Pages;
+using TodoApp.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<TodoAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoAppContext") ?? throw new InvalidOperationException("Connection string 'TodoAppContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TodoApp.Client._Imports).Assembly);
+
+app.Run();
